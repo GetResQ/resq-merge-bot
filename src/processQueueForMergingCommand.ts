619e533dcdde6fa061f6db1bc04220ee040596a6
abdullahs26 <ashahid@getresq.com>
2024-02-08 12:32:55 -0500

    test log

--- src/processQueueForMergingCommand.ts
+++ src/processQueueForMergingCommand.ts
@@ -94,10 +94,10 @@ export async function processQueueForMergingCommand(
       headRef: { name: pr.head.ref },
     })
     core.info("Make PR up-to-date")
+    core.info("test log")
   } catch (error) {
     if (error.message === 'Failed to merge: "Already merged"') {
       core.info("PR already up-to-date.")
-      core.info(pr.title)
       try {
         core.info(pr.title)
         await mergePr({
