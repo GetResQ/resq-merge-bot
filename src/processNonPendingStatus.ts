fe2a92704d98cb7563ab3783c874971403ca3419
abdullahs26 <ashahid@getresq.com>
2024-02-13 11:07:04 -0500

    Revert "Revert "v0.0.2""
    This reverts commit 5694428ae539382dfccdbf7ae1b2d92aeb7c9c6b.

--- src/processNonPendingStatus.ts
+++ src/processNonPendingStatus.ts
@@ -21,7 +21,6 @@ export async function processNonPendingStatus(
 ): Promise<void> {
   const {
     repository: {
-      branchProtectionRules,
       labels: { nodes: labelNodes },
     },
   } = await fetchData(repo.owner.login, repo.name)
@@ -35,31 +34,17 @@ export async function processNonPendingStatus(
 
   const mergingPr = mergingLabel.pullRequests.nodes[0]
   const latestCommit = mergingPr.commits.nodes[0].commit
-  if (commit.node_id !== latestCommit.id) {
-    // Commit that trigger this hook is not the latest commit of the merging PR
-    return
-  }
-  const baseBranchRule = branchProtectionRules.nodes.find(
-    (rule) => rule.pattern === mergingPr.baseRef.name
-  )
-  if (!baseBranchRule) {
-    // TODO: No protection rule for merging this PR. Merge immediately?
-    return
-  }
-  const requiredCheckNames = baseBranchRule.requiredStatusCheckContexts
 
   if (state === "success") {
-    const isAllRequiredCheckPassed = requiredCheckNames.every((checkName) => {
-      if (!checkName.includes("ci/circleci")) {
-        // TODO: Support GitHub Action. Can't get `statusCheckRollup` to work in GitHub API Explorer for some reason.
-        return true
+    const isAllRequiredCheckPassed = latestCommit.checkSuites.nodes.every(
+      (node) => {
+        let status = node.checkRuns.nodes[0]?.status
+        if (node.checkRuns.nodes[0]?.name === "merge-queue") {
+          status = "COMPLETED"
+        }
+        return status === "COMPLETED" || status === null || status === undefined
       }
-      return latestCommit.status.contexts.find(
-        (latestCommitContext) =>
-          latestCommitContext.context === checkName &&
-          latestCommitContext.state === "SUCCESS"
-      )
-    })
+    )
     if (!isAllRequiredCheckPassed) {
       core.info("Not all Required Checks have finished.")
       return
@@ -71,11 +56,6 @@ export async function processNonPendingStatus(
     } catch (error) {
       core.info("Unable to merge the PR.")
       core.error(error)
-    }
-  } else {
-    if (!requiredCheckNames.includes(context)) {
-      // The failed check from this webhook is not in the required status check, so we can ignore it.
-      return
     }
   }
 
@@ -102,9 +82,6 @@ async function fetchData(
   repo: string
 ): Promise<{
   repository: {
-    branchProtectionRules: {
-      nodes: { pattern: string; requiredStatusCheckContexts: string[] }[]
-    }
     labels: {
       nodes: {
         id: string
@@ -141,47 +118,44 @@ async function fetchData(
 }> {
   return graphqlClient(
     `query allLabels($owner: String!, $repo: String!) {
-         repository(owner:$owner, name:$repo) {
-           branchProtectionRules(last: 10) {
-             nodes {
-               pattern
-               requiredStatusCheckContexts
-             }
-           }
-           labels(last: 50) {
-             nodes {
-               id
-               name
-               pullRequests(first: 20) {
-                 nodes {
-                   id
-                   number
-                   title
-                   baseRef {
-                     name
-                   }
-                   headRef {
-                     name
-                   }
-                   commits(last: 1) {
-                     nodes {
-                       commit {
-                         id
-                         status {
-                           contexts {
-                             context
-                             state
+      repository(owner:$owner, name:$repo) {
+        labels(last: 50) {
+          nodes {
+            id
+            name
+            pullRequests(first: 20) {
+              nodes {
+                id
+                number
+                title
+                baseRef {
+                  name
+                }
+                headRef {
+                  name
+                }
+                commits(last: 1) {
+                  nodes {
+                   commit {
+                     checkSuites(first: 10) {
+                       nodes {
+                         checkRuns(first:10) {
+                           nodes {
+                             status
+                             name
                            }
                          }
                        }
                      }
                    }
-                 }
-               }
-             }
-           }
-         }
-       }`,
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }`,
     { owner, repo }
   )
 }
